3bf7bd1d9a326250bd0382064bdd8a0dc752eda7
NJCR13 <up201906051@up.pt>
2023-04-14 16:44:16 +0100

    Started devolping main screen
    Tab bar done

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -6,7 +6,14 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => mainpage()),
+          );
+        },
+        child: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [Colors.pink, Colors.deepPurple],
@@ -15,32 +22,74 @@ class WelcomePage extends StatelessWidget {
             ),
           ),
           child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Welcome to TasteBudz :)',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Image.asset(
+                  'assets/images/happy.gif',
+                  height: 200,
+                  width: 200,
+                ),
+                const SizedBox(height: 40),
+                const Text(
+                  'You are now logged in!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      // Moved closing parenthesis here
+    );
+  }
+}
+
+class mainpage extends StatelessWidget {
+  const mainpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: SizedBox.shrink(),
+          ),
+          body: TabBarView(
             children: [
-              const Text(
-                'Welcome to TasteBudz :)',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 40),
-              Image.asset(
-                'assets/images/happy.gif',
-                height: 200,
-                width: 200,
-              ),
-              const SizedBox(height: 40),
-              const Text(
-                'You are now logged in!',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-              ),
+              Icon(Icons.home_outlined),
+              Icon(Icons.favorite_border_outlined),
+              Icon(Icons.circle_outlined),
+              Icon(Icons.person_outline),
             ],
+          ),
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home_outlined)),
+              Tab(icon: Icon(Icons.favorite_border_outlined)),
+              Tab(icon: Icon(Icons.circle_outlined)),
+              Tab(icon: Icon(Icons.person_outline)),
+            ],
+            labelColor: Color(0xFFFFB080),
+            unselectedLabelColor: Colors.grey,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Color(0xFFFFB080),
           ),
         ),
       ),
