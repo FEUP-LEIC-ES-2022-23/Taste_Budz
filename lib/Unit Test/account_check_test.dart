6e25714b49adcb37c7ded915eb6758cc4fc4da72
acarolinacc <acarolinacc2003@gmail.com>
2023-04-19 20:47:46 +0100

    Account check unit test

---
+++ lib/Unit Test/account_check_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../components/already_have_an_account_acheck.dart';
+
+void main() {
+  group('AlreadyHaveAnAccountCheck', () {
+    testWidgets('displays "Don’t have an Account ? " when login is true',
+            (WidgetTester tester) async {
+          await tester.pumpWidget(MaterialApp(
+            home: AlreadyHaveAnAccountCheck(login: true, press: () {}),
+          ));
+          expect(find.text('Don’t have an Account ? '), findsOneWidget);
+        });
+
+    testWidgets('displays "Already have an Account ? " when login is false',
+            (WidgetTester tester) async {
+          await tester.pumpWidget(MaterialApp(
+            home: AlreadyHaveAnAccountCheck(login: false, press: () {}),
+          ));
+          expect(find.text('Already have an Account ? '), findsOneWidget);
+        });
+
+    testWidgets('calls press callback when tapped', (WidgetTester tester) async {
+      var pressed = false;
+      await tester.pumpWidget(MaterialApp(
+        home: AlreadyHaveAnAccountCheck(login: true, press: () => pressed = true),
+      ));
+      await tester.tap(find.text('Sign Up'));
+      expect(pressed, isTrue);
+
+      await tester.pumpWidget(MaterialApp(
+        home: AlreadyHaveAnAccountCheck(login: false, press: () => pressed = true),
+      ));
+      await tester.tap(find.text('Log In'));
+      expect(pressed, isTrue);
+    });
+  });
+}
