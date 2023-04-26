118a713e587237d05d93442b31789930ecf45e64
acarolinacc <acarolinacc2003@gmail.com>
2023-04-26 19:01:56 +0100

    now the user can share their city in the share location screen

--- lib/Screens/Welcome/share_location.dart
+++ lib/Screens/Welcome/share_location.dart
@@ -9,8 +9,10 @@ class Androidlarge39Widget extends StatefulWidget {
 }
 
 class _Androidlarge39WidgetState extends State<Androidlarge39Widget> {
+  String _city = '';
   @override
   Widget build(BuildContext context) {
+
     const titleText = 'Share your location with us !';
     final titleTextStyle = TextStyle(
       color: Color.fromRGBO(50, 50, 77, 1),
@@ -21,7 +23,7 @@ class _Androidlarge39WidgetState extends State<Androidlarge39Widget> {
       height: 1.25,
     );
     final descriptionText =
-        'Please enter your location or allow access to your location to find all restaurants that are near you ';
+        'Let us help you find great dining options in your area. Please enter your location.';
     final descriptionTextStyle = TextStyle(
       color: Color.fromRGBO(142, 142, 169, 1),
       fontFamily: 'Mulish',
@@ -37,290 +39,97 @@ class _Androidlarge39WidgetState extends State<Androidlarge39Widget> {
         decoration: BoxDecoration(),
         padding: EdgeInsets.symmetric(horizontal: 0, vertical: 100),
         child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+        Container(
+        decoration: BoxDecoration(),
+        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+        child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(),
-              padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    titleText,
-                    textAlign: TextAlign.center,
-                    style: titleTextStyle,
-                  ),
-                  SizedBox(height: 14),
-                  Text(
-                    descriptionText,
-                    textAlign: TextAlign.center,
-                    style: descriptionTextStyle,
-                  ),
-                ],
-              ),
+            Text(
+              titleText,
+              textAlign: TextAlign.center,
+              style: titleTextStyle,
             ),
-            Container(
-              width: 320,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(50, 50, 71, 0.03999999910593033),
-                    offset: Offset(0, 4),
-                    blurRadius: 20,
-                  ),
-                ],
-                color: Color.fromRGBO(255, 255, 255, 1),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Current Location',
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  color: Color.fromRGBO(33, 33, 52, 1),
-                                  fontFamily: 'Mulish',
-                                  fontSize: 16,
-                                  letterSpacing: 0,
-                                  fontWeight: FontWeight.normal,
-                                  height: 1.375,
-                                ),
-                              ),
-                              SizedBox(height: 12),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    '794 8th Ave, New York',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      color: Color.fromRGBO(142, 142, 169, 1),
-                                      fontFamily: 'Mulish',
-                                      fontSize: 14,
-                                      letterSpacing: 0,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1.7142857142857142,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 88),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255, 255, 255, 1),
-                            border: Border.all(
-                              color: Color.fromRGBO(192, 192, 207, 1),
-                              width: 1,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.elliptical(20, 20)),
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                          child: Container(
-                            width: 10,
-                            height: 10,
-                            decoration: BoxDecoration(
-                              color: const Color(0xFFFFB080),
-                              borderRadius: BorderRadius.all(Radius.elliptical(10, 10)),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+            SizedBox(height: 14),
+            Text(
+              descriptionText,
+              textAlign: TextAlign.center,
+              style: descriptionTextStyle,
             ),
-            Container(
-              width: 320,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(50, 50, 71, 0.03999999910593033),
-                    offset: Offset(0, 4),
-                    blurRadius: 20,
-                  ),
-                ],
-                color: Color.fromRGBO(255, 255, 255, 1),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'My home',
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  color: Color.fromRGBO(33, 33, 52, 1),
-                                  fontFamily: 'Mulish',
-                                  fontSize: 16,
-                                  letterSpacing: 0,
-                                  fontWeight: FontWeight.normal,
-                                  height: 1.375,
-                                ),
-                              ),
-                              SizedBox(height: 12),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    '284 2th Ave, New York',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      color: Color.fromRGBO(142, 142, 169, 1),
-                                      fontFamily: 'Mulish',
-                                      fontSize: 14,
-                                      letterSpacing: 0,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1.7142857142857142,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 88),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255, 255, 255, 1),
-                            border: Border.all(
-                              color: Color.fromRGBO(192, 192, 207, 1),
-                              width: 1,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.elliptical(20, 20)),
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 9, vertical: 9),
-
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Enter the city you are in:',
+                style: TextStyle(
+                  color: Color.fromRGBO(142, 142, 169, 1),
+                  fontFamily: 'Mulish',
+                  fontSize: 16,
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                  height: 1.75,
+                )
             ),
-            Container(
-              width: 320,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(50, 50, 71, 0.03999999910593033),
-                    offset: Offset(0, 4),
-                    blurRadius: 20,
+            SizedBox(height: 8.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'City',
+                hintText: 'e.g. Porto',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 1.0,
                   ),
-                ],
-                color: Color.fromRGBO(255, 255, 255, 1),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Sushi Bar',
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  color: Color.fromRGBO(33, 33, 52, 1),
-                                  fontFamily: 'Mulish',
-                                  fontSize: 16,
-                                  letterSpacing: 0,
-                                  fontWeight: FontWeight.normal,
-                                  height: 1.375,
-                                ),
-                              ),
-                              SizedBox(height: 12),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    '423 8th Ave, New York',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      color: Color.fromRGBO(142, 142, 169, 1),
-                                      fontFamily: 'Mulish',
-                                      fontSize: 14,
-                                      letterSpacing: 0,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1.7142857142857142,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 88),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255, 255, 255, 1),
-                            border: Border.all(
-                              color: Color.fromRGBO(192, 192, 207, 1),
-                              width: 1,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.elliptical(20, 20)),
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 9, vertical: 9),
-
-                        ),
-                      ],
-                    ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 1.0,
                   ),
-                ],
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).accentColor,
+                    width: 2.0,
+                  ),
+                ),
               ),
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _city = value;
+                });
+              },
             ),
 
             SizedBox(
-              height: MediaQuery.of(context).size.height* 0.09,
+              height: MediaQuery.of(context).size.height* 0.34,
             ),
             SizedBox(
               width: MediaQuery.of(context).size.width* 0.8,
               child: ElevatedButton(
                 onPressed: () {
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
+                    context,
+                    MaterialPageRoute(
                       builder: (context) {
-                    return EnjoyScreen();
-                  },
-                  ),
+                        return EnjoyScreen();
+                      },
+                    ),
                   );
                 },
                 style: ElevatedButton.styleFrom(
@@ -337,10 +146,11 @@ class _Androidlarge39WidgetState extends State<Androidlarge39Widget> {
               ),
             ),
           ],
-
-
         ),
       ),
+    ]
+    )
+    )
     );
   }
 }
