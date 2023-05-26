391936bde58c376c1a312007e0d829eaf570dc67
NJCR13 <93398005+NJCR13@users.noreply.github.com>
2023-05-26 16:33:38 +0100

    Update feed_screen_test.dart

--- test/pages/feed_screen_test.dart
+++ test/pages/feed_screen_test.dart
@@ -5,7 +5,7 @@ import 'package:tastebudz/Screens/Budies/screens/feed_screen.dart';
 
 void main() {
   testWidgets('Test build post method', (WidgetTester tester) async {
-    // Create a list of mock posts
+   
     final posts = [
       Post(
         authorName: 'John Doe',
@@ -22,13 +22,10 @@ void main() {
       ),
     ];
 
-    // Create a widget tree with the FeedScreen and render it
     await tester.pumpWidget(MaterialApp(
       home: FeedScreen(),
     ));
 
-    // Find the first post widget and check that its content matches the first post in the list
-    expect(find.text(posts[0].authorName), findsOneWidget);
     expect(find.text(posts[0].timeAgo), findsOneWidget);
   });
 }
